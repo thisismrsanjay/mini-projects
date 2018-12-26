2a5f99b191623187ad24e7111eab05b4ce979a64
thisismrsanjay <37665041+thisismrsanjay@users.noreply.github.com>
2018-12-26 16:07:11 +0530

    storage added

--- BookList/app.js
+++ BookList/app.js
@@ -10,19 +10,7 @@ class Book {
 //UI Class: Handle UI Tasks
 class UI {
     static displayBooks() {
-        const StoredBooks = [
-            {
-                title: 'Book One',
-                author: 'Jhon Doe',
-                isbn: '2323232'
-            },
-            {
-                title: 'Book Two',
-                author: 'Twin Doe',
-                isbn: '2323231'
-            }
-        ]
-        const books = StoredBooks;
+        const books = Store.getBooks();
 
         books.forEach((book) => UI.addBookToList(book));
     }
@@ -50,6 +38,12 @@ class UI {
         const div =document.createElement('div');
         div.className=`alert alert-${className}`;
         div.appendChild(document.createTextNode(message));
+        const container = document.querySelector('.container');
+        const form = document.querySelector('#book-form');
+        container.insertBefore(div,form);
+
+        //vanish in 3 second
+        setTimeout(()=>document.querySelector('.alert').remove(),3000);
     }
 
     static clearFields() {
@@ -60,6 +54,32 @@ class UI {
 }
 
 //Store Class: Handles Storage
+
+class Store{
+    static getBooks(){
+        let books;
+        if(localStorage.getItem('books')===null){
+            books=[];
+        }else{
+            books=JSON.parse(localStorage.getItem('books'));
+        }
+        return books;
+    }
+    static addBook(book){
+        const books = Store.getBooks();
+        books.push(book);
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+    static removeBook(isbn){
+        const books =Store.getBooks();
+        books.forEach((book,index)=>{
+            if(book.isbn==isbn){
+                books.splice(index,1);
+            }
+        });
+        localStorage.setItem('books',JSON.stringify(books));
+    }
+}
 
 //Event: Display Books
 document.addEventListener('DOMContentLoaded', UI.displayBooks);
@@ -73,13 +93,19 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 
     //Validate 
     if (title === '' || author === '' || isbn === "") {
-        alert('Please fill in all fields')
+        UI.showAlert('Please fill in all fields','danger');
     } else {
         //Instantiate book
         const book = new Book(title, author, isbn);
 
         //Add Book to UI
         UI.addBookToList(book);
+
+        //Add book to store
+        Store.addBook(book);
+
+        //Show success message
+        UI.showAlert('Book Added','success');
 
         //Clear fields
         UI.clearFields();
@@ -90,5 +116,12 @@ document.querySelector('#book-form').addEventListener('submit', (e) => {
 
 //Event:Remove a Book
 document.querySelector('#book-list').addEventListener('click', (e) => {
+    //Remove book from UI
     UI.deleteBook(e.target);
+
+    //remove book from store
+    Store.removeBook(e.target.parentElement.previousElementSibling.textContent);
+
+    //Show delete message
+    UI.showAlert('Book Removed','success');
 })
--- BookList/readme.md
+++ BookList/readme.md
@@ -1,2 +1,5 @@
+classes UI STORAGE  
 create   
 delete in vannila js  
+![git hub ](https://github.com/thisismrsanjay/mini-projects/blob/master/BookList/Capture.PNG)  
+https://thisismrsanjay.github.io/mini-projects/BookList
